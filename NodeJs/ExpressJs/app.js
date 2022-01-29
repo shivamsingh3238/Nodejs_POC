87667d5f15432952ef3e621e59a1201657a9fab0
SHIVAM SINGH <shivam.cse98@gmail.com>
2022-01-29 11:12:18 +0530

    update

--- NodeJs/ExpressJs/app.js
+++ NodeJs/ExpressJs/app.js
@@ -2,9 +2,14 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
+app.use("/test", (req, res, next) => {
+  console.log("this is first midleware");
+  res.send("<h1>Hi this is new route</h1>");
+});
+
+app.use("/", (req, res, next) => {
   console.log("this is secound midleware");
-  res.send("<h1>Hi this is from express</h1>");
+  res.send("<h1>Hi this is / route</h1>");
   next();
 });
 
